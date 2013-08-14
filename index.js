b4b2c288a895ff2cf83ebd981de7b8996ef23c2d
Feross Aboukhadijeh <feross@feross.org>
2013-08-13 17:17:34 -0700

    report unexpected response

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ var current = process.versions.node
 
 request.get(url, function (err, res, body) {
   if (err) {
-    console.error('Cannot connect to nodejs.org to get latest version.')
+    console.error('Nagger cannot connect to nodejs.org to get latest version.')
   } else {
-    var latest = /node-v(\d+\.\d+\.\d+)/.exec(body)[1]
-    if (latest !== current) {
-      console.log('===================================================')
-      console.log(' WARNING: Installed Node version is out-of-date!   ')
-      console.log(' (Current: ' + current + ' Latest: ' + latest + ') ')
-      console.log('===================================================')
+    var latest = /node-v(\d+\.\d+\.\d+)/.exec(body)
+    if (latest) {
+      if (latest[1] !== current) {
+        console.log('===================================================')
+        console.log(' WARNING: Installed Node version is out-of-date!   ')
+        console.log(' (Current: ' + current + ' Latest: ' + latest + ') ')
+        console.log('===================================================')
+      }
+    } else {
+      console.log('Nagger got unexpected response from nodejs.org. Are you on free wifi? Try logging in.')
     }
   }
 })
