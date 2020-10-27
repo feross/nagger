7c25cde4d3ddd96e0807a679cbe36d3d82dd69ba
Feross Aboukhadijeh <feross@feross.org>
2020-10-26 23:55:57 -1000

    separate module and bin

---
+++ bin/cmd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+var nagger = require('nagger')
+
+nagger()
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*! nagger. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
-#!/usr/bin/env node
 
 var boxen = require('boxen')
 var get = require('simple-get')
@@ -8,23 +7,27 @@ var chalk = require('chalk')
 var url = 'http://nodejs.org/dist/latest/SHASUMS256.txt'
 var currentVersion = process.versions.node
 
-get.concat(url, function (err, res, body) {
-  if (err) {
-    console.log(chalk.red.bold('\n Nagger couldn\'t connect to nodejs.org to get the latest version.\n'))
-    console.error(err)
-    process.exitCode = 1
-    return
-  }
+function nagger () {
+  get.concat(url, function (err, res, body) {
+    if (err) {
+      console.log(chalk.red.bold('\n Nagger couldn\'t connect to nodejs.org to get the latest version.\n'))
+      console.error(err)
+      process.exitCode = 1
+      return
+    }
 
-  var latestVersion = /node-v(\d+\.\d+\.\d+)/.exec(body)
-  latestVersion = latestVersion && latestVersion[1]
+    var latestVersion = /node-v(\d+\.\d+\.\d+)/.exec(body)
+    latestVersion = latestVersion && latestVersion[1]
 
-  var currentMessage = chalk.green.bold(` Current : ${currentVersion}`)
-  var latestMessage = chalk.cyan.bold(` Latest : ${latestVersion}`)
+    var currentMessage = chalk.green.bold(` Current : ${currentVersion}`)
+    var latestMessage = chalk.cyan.bold(` Latest : ${latestVersion}`)
 
-  if (latestVersion !== currentVersion) {
-    console.log(chalk.red.bold(boxen(chalk.red.bold(
-      ` WARNING: Installed Node version is out-of-date \n\n${currentMessage}                ${latestMessage}`
-    ))))
-  }
-})
+    if (latestVersion !== currentVersion) {
+      console.log(chalk.red.bold(boxen(chalk.red.bold(
+        ` WARNING: Installed Node version is out-of-date \n\n${currentMessage}                ${latestMessage}`
+      ))))
+    }
+  })
+}
+
+module.exports = nagger
